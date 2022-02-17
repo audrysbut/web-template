785831bb01b20e12d9a8986576b19ee4e34348f6
Audrius Butkus <audrius.butkus@telesoftas.com>
2022-02-17 21:51:34 +0200

    chore: added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+function One() {
+  return <div>Comp1</div>;
+}
+
+function Two() {
+  return <div>Comp2</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <div>
+        <div>Pages: </div>
+        <Switch>
+          <Route exact path="/">
+            <One />
+          </Route>
+          <Route path="/two">
+            <Two />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
